7cd1dfe5fe2c91ba3357eb85a2e4688afba72b62
Kazuki Oishi <oishikazuki@gmail.com>
2022-10-17 04:28:24 +0900

    add libs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+const withTranspileModules = require("next-transpile-modules")
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = () => {
+  const plugins = [withTranspileModules(["@cloudscape-design/components"])]
+  return plugins.reduce((acc, plugin) => plugin(acc), nextConfig)
+}
