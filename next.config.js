8d27be88bcbfcdd0a0cf2d4a109d9d8463873288
Kazuki Oishi <oishikazuki@gmail.com>
2022-10-28 07:50:17 +0900

    enable standalone output

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withTranspileModules = require('next-transpile-modules')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
   swcMinify: true,
   compiler: {
