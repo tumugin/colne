d3b6da525f4dca9fc6a582f339d1a79e1a7fb737
Kazuki Oishi <oishikazuki@gmail.com>
2022-11-10 23:22:59 +0900

    add redux-toolkit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,8 +5,18 @@ import { AppLayout, TopNavigation } from '@cloudscape-design/components'
 import React, { useCallback, useEffect, useState } from 'react'
 import { applyMode, Mode } from '@cloudscape-design/global-styles'
 import { useDarkTheme } from 'libs/dom/useDarkTheme'
+import { store } from '../store'
+import { Provider } from 'react-redux'
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function ColneApp(props: AppProps) {
+  return (
+    <Provider store={store}>
+      <AppWithLayout {...props} />
+    </Provider>
+  )
+}
+
+function AppWithLayout({ Component, pageProps }: AppProps) {
   const [navigationOpen, setNavigationOpen] = useState(false)
   const isDarkTheme = useDarkTheme()
   const toggleNavigation = useCallback(() => {
@@ -41,5 +51,3 @@ function MyApp({ Component, pageProps }: AppProps) {
     </>
   )
 }
-
-export default MyApp
