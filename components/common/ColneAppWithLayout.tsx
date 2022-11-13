fcca9d29e21ee79afa8980b05d1bbb7bee79a336
Kazuki Oishi <oishikazuki@gmail.com>
2022-11-14 03:15:40 +0900

    右上の表示を実装

--- components/common/ColneAppWithLayout.tsx
+++ components/common/ColneAppWithLayout.tsx
@@ -5,6 +5,8 @@ import { useAppSelector } from 'store'
 import { useCSRFToken } from 'store/common/commonHooks'
 import { ColneSideNavigation } from 'components/common/ColneSideNavigation'
 import { useRouter } from 'next/router'
+import { loginPath } from 'utils/urls'
+import { useLogoutForm } from 'components/common/LogoutForm'
 
 export function ColneAppWithLayout({ Component, pageProps }: AppProps) {
   const router = useRouter()
@@ -13,11 +15,14 @@ export function ColneAppWithLayout({ Component, pageProps }: AppProps) {
     setNavigationOpen((prev) => !prev)
   }, [])
 
-  const isLoggedIn = !!useAppSelector((state) => state.user.currentUser)
+  const user = useAppSelector((state) => state.user.currentUser)
+  const isLoggedIn = !!user
   const csrfToken = useCSRFToken()
+  const [logoutFormElement, triggerLogout] = useLogoutForm({ csrfToken })
 
   return (
     <>
+      {logoutFormElement}
       <TopNavigation
         identity={{
           href: '/',
@@ -35,6 +40,26 @@ export function ColneAppWithLayout({ Component, pageProps }: AppProps) {
           overflowMenuBackIconAriaLabel: 'Back',
           overflowMenuDismissIconAriaLabel: 'Close menu',
         }}
+        utilities={[
+          {
+            type: 'menu-dropdown',
+            text: user?.userName ?? 'ゲスト',
+            iconName: 'user-profile',
+            items: [
+              ...(isLoggedIn
+                ? ([{ id: 'logout', text: 'ログアウト' }] as const)
+                : ([
+                    { id: 'login', href: loginPath, text: 'ログイン' },
+                    { id: 'register', href: loginPath, text: '新規登録' },
+                  ] as const)),
+            ],
+            onItemClick: (e) => {
+              if (e.detail.id === 'logout') {
+                triggerLogout()
+              }
+            },
+          },
+        ]}
       />
       <AppLayout
         content={<Component {...pageProps} />}
