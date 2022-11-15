6c046ada074a7dd75c76215d1ac65ac4f7d914b8
Kazuki Oishi <oishikazuki@gmail.com>
2022-11-15 18:07:29 +0000

    処理を追加

--- store/cheki/chekiHooks.ts
+++ store/cheki/chekiHooks.ts
@@ -1,11 +1,29 @@
 import { AppDispatch } from 'store/index'
 import dayjs from 'dayjs'
+import { colneGraphQLSdk } from 'graphql/client'
+import { chekiSlice } from './chekiStore'
 
-export async function updateUserChekiIdolCount(
+export async function updateCurrentUserChekiIdolCount(
   params: {
     startDate: dayjs.Dayjs
     endDate: dayjs.Dayjs
   },
   dispatch: AppDispatch,
   headers?: Record<string, string>
-) {}
+) {
+  const result = await colneGraphQLSdk.GetUserChekiIdolCount(
+    {
+      chekiShotAtStart: params.startDate.toISOString(),
+      chekiShotAtEnd: params.endDate.toISOString(),
+    },
+    headers
+  )
+  await dispatch(
+    chekiSlice.actions.updateCurrentUserChekiIdolCount(
+      result.currentUserChekis.getUserChekiIdolCount.map((p) => ({
+        chekiCount: p.chekiCount,
+        idol: p.idol ?? null,
+      }))
+    )
+  )
+}
--- store/cheki/chekiStore.ts
+++ store/cheki/chekiStore.ts
@@ -1,11 +1,30 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
-interface State {}
+interface State {
+  currentUserChekiIdolCount:
+    | {
+        chekiCount: number
+        idol: {
+          idolId: string
+          idolName: string
+        } | null
+      }[]
+    | null
+}
 
-const initialState: State = {}
+const initialState: State = {
+  currentUserChekiIdolCount: null,
+}
 
 export const chekiSlice = createSlice({
   name: 'cheki',
   initialState,
-  reducers: {},
+  reducers: {
+    updateCurrentUserChekiIdolCount(
+      state,
+      action: PayloadAction<State['currentUserChekiIdolCount']>
+    ) {
+      state.currentUserChekiIdolCount = action.payload
+    },
+  },
 })
