32bd59738ab570dfd1104a630779a0d80da5df13
Kazuki Oishi <oishikazuki@gmail.com>
2023-01-09 06:35:05 +0900

    ライブラリを使うよ

--- components/idols/create/IdolCreateForm.tsx
+++ components/idols/create/IdolCreateForm.tsx
@@ -28,6 +28,7 @@ export function IdolCreateForm() {
       name: '',
       idolStatus: 'private',
     },
+    mode: 'all',
   })
 
   return (
@@ -44,22 +45,37 @@ export function IdolCreateForm() {
           }
         >
           <SpaceBetween direction="vertical" size="m">
-            <FormField label="アイドル名">
-              <Controller
-                name="name"
-                control={control}
-                rules={{ required: true, min: 1, max: 255 }}
-                render={({ field, fieldState }) => (
-                  <Input {...field} autoFocus invalid={!!fieldState.error} />
-                )}
-              />
-            </FormField>
-            <FormField label="アイドルの公開状態">
-              <Controller
-                name="idolStatus"
-                control={control}
-                rules={{ required: true }}
-                render={({ field, fieldState }) => (
+            <Controller
+              name="name"
+              control={control}
+              rules={{
+                required: 'アイドル名の入力は必須です',
+                maxLength: { value: 255, message: 'アイドル名の入力上限は255文字です' },
+              }}
+              render={({ field, fieldState }) => (
+                <FormField
+                  label="アイドル名"
+                  errorText={fieldState.error && fieldState.error.message}
+                >
+                  <Input
+                    onChange={(e) => field.onChange(e.detail.value)}
+                    value={field.value}
+                    onBlur={field.onBlur}
+                    invalid={!!fieldState.error}
+                  />
+                </FormField>
+              )}
+            />
+
+            <Controller
+              name="idolStatus"
+              control={control}
+              rules={{ required: true }}
+              render={({ field, fieldState }) => (
+                <FormField
+                  label="アイドルの公開状態"
+                  errorText={fieldState.error && fieldState.error.message}
+                >
                   <Select
                     selectedOption={
                       idolStatusOptions.find((x) => x.value === field.value)!
@@ -70,9 +86,9 @@ export function IdolCreateForm() {
                     options={idolStatusOptions}
                     invalid={!!fieldState.error}
                   />
-                )}
-              />
-            </FormField>
+                </FormField>
+              )}
+            />
             <Alert>
               アイドルの公開状態を公開状態にすると他のユーザからの参照が可能になります
             </Alert>
