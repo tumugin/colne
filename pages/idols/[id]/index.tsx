c7f0e39237276994554d1b4176e99b6a44f20074
Kazuki Oishi <oishikazuki@gmail.com>
2023-01-15 09:23:25 +0900

    詳細コンポーネントを追加する

--- pages/idols/[id]/index.tsx
+++ pages/idols/[id]/index.tsx
@@ -1,40 +1,67 @@
 import { NextPage } from 'next'
 import { ContentLayout } from '@cloudscape-design/components'
 import { getRequestHeaderFromContext } from 'utils/headers'
-import { wrapper } from 'store'
+import { useAppSelector, wrapper } from 'store'
 import { getIdol } from 'store/idol/idolHooks'
 import {
   ErrorAwarePageProps,
   handleExceptionAndReturnErrorAwarePageProps,
 } from 'utils/error-aware-page-utils'
 import Error from 'next/error'
+import { IdolDetailsView } from 'components/idols/IdolDetailsView'
 
-interface Props extends ErrorAwarePageProps {}
+interface Props extends ErrorAwarePageProps {
+  idolId: string
+}
 
 const IdolDetails: NextPage<Props> = (props) => {
+  const idol = useAppSelector((state) => state.idol.idols[props.idolId])
+
   if (props.error) {
     return <Error statusCode={props.error.statusCode} />
   }
 
-  return <ContentLayout></ContentLayout>
+  return (
+    <ContentLayout>
+      {idol && (
+        <IdolDetailsView
+          idol={{
+            name: idol.idolName,
+            id: idol.idolId,
+            status: idol.idolStatus,
+            groups: idol.groups.map((group) => ({
+              id: group.groupId,
+              name: group.groupName,
+            })),
+          }}
+        />
+      )}
+    </ContentLayout>
+  )
 }
 
 IdolDetails.getInitialProps = wrapper.getInitialPageProps(
   (store) =>
     async (ctx): Promise<Props> => {
-      if (store.getState().idol.idols[ctx.query.id as string]) {
-        return {}
+      const idolId = ctx.query.id as string
+      if (store.getState().idol.idols[idolId]) {
+        return {
+          idolId,
+        }
       }
       try {
         await getIdol(
           store.dispatch,
-          { idolId: ctx.query.id as string },
+          { idolId },
           getRequestHeaderFromContext(ctx)
         )
       } catch (e) {
-        return handleExceptionAndReturnErrorAwarePageProps(e, ctx)
+        return {
+          ...handleExceptionAndReturnErrorAwarePageProps(e, ctx),
+          idolId,
+        }
       }
-      return {}
+      return { idolId }
     }
 )
 
