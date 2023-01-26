601e4ed1dd57fd36ff8e5dc1b67752ac8b03af3f
Kazuki Oishi <oishikazuki@gmail.com>
2023-01-26 18:42:05 +0900

    ボタンが動くようにする

--- components/chekis/ChekiAddCounter.tsx
+++ components/chekis/ChekiAddCounter.tsx
@@ -1,5 +1,5 @@
 import { Icon, Input } from '@cloudscape-design/components'
-import React from 'react'
+import React, { useCallback } from 'react'
 import styled from 'styled-components'
 import * as awsui from '@cloudscape-design/design-tokens'
 
@@ -31,26 +31,50 @@ const CustomBigNumberInput = styled.input`
   font-size: ${awsui.spaceScaledXxl};
   text-align: center;
   width: 100px;
+
+  &::-webkit-outer-spin-button,
+  &::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
 `
 
 export function ChekiAddCounter({
   countValue,
   minValue,
+  maxValue,
+  onChange,
 }: {
   countValue: number
   minValue?: number
+  maxValue?: number
+  onChange?: (value: number) => void
 }) {
+  const handleOnIncrease = useCallback(() => {
+    onChange && onChange(countValue + 1)
+  }, [countValue, onChange])
+  const handleOnDecrease = useCallback(() => {
+    onChange && onChange(countValue - 1)
+  }, [countValue, onChange])
+
   return (
     <Wrapper>
-      <CustomBigButton>
+      <CustomBigButton
+        onClick={handleOnDecrease}
+        disabled={minValue ? countValue - 1 < minValue : false}
+      >
         <Icon name="angle-down" size="big" variant="normal" />
       </CustomBigButton>
       <CustomBigNumberInput
         value={countValue.toString()}
         type="number"
         min={minValue}
+        max={maxValue}
       />
-      <CustomBigButton>
+      <CustomBigButton
+        onClick={handleOnIncrease}
+        disabled={maxValue ? countValue + 1 > maxValue : false}
+      >
         <Icon name="angle-up" size="big" variant="normal" />
       </CustomBigButton>
     </Wrapper>
--- stories/chekis/ChekiAddCounter.stories.tsx
+++ stories/chekis/ChekiAddCounter.stories.tsx
@@ -1,5 +1,6 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 import { ChekiAddCounter } from 'components/chekis/ChekiAddCounter'
+import { useState } from 'react'
 
 const componentMeta: ComponentMeta<typeof ChekiAddCounter> = {
   title: 'Chekis/ChekiAddCounter',
@@ -8,13 +9,21 @@ const componentMeta: ComponentMeta<typeof ChekiAddCounter> = {
 
 export default componentMeta
 
-const Template: ComponentStory<typeof ChekiAddCounter> = (args) => (
-  <ChekiAddCounter {...args} />
-)
+const Template: ComponentStory<typeof ChekiAddCounter> = (args) => {
+  const [countValue, setCountValue] = useState(args.countValue)
+  return (
+    <ChekiAddCounter
+      {...args}
+      countValue={countValue}
+      onChange={setCountValue}
+    />
+  )
+}
 
 export const Primary = Template.bind({})
 
 Primary.args = {
   countValue: 1,
   minValue: 1,
+  maxValue: 1000,
 }
