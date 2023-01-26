50a22bb92ee8134691e3726700a4e3bc1331a719
Kazuki Oishi <oishikazuki@gmail.com>
2023-01-27 02:11:33 +0900

    add test

--- components/chekis/ChekiAddCounter.cy.tsx
+++ components/chekis/ChekiAddCounter.cy.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { ChekiAddCounter } from 'components/chekis/ChekiAddCounter'
-import { stubDarkMode } from "test-utils/cypress-darkmode";
 
 describe('<ChekiAddCounter>', () => {
   it('mounts with smartphone', () => {
@@ -10,22 +9,6 @@ describe('<ChekiAddCounter>', () => {
   })
 
   it('mounts with pc', () => {
-    cy.viewport('macbook-16')
-    cy.mount(<ChekiAddCounter countValue={123} />)
-    cy.matchImage()
-  })
-})
-
-describe('<ChekiAddCounter> dark mode', () => {
-  it('mounts with smartphone', () => {
-    stubDarkMode(cy)
-    cy.viewport('iphone-xr')
-    cy.mount(<ChekiAddCounter countValue={123} />)
-    cy.matchImage()
-  })
-
-  it('mounts with pc', () => {
-    stubDarkMode(cy)
     cy.viewport('macbook-16')
     cy.mount(<ChekiAddCounter countValue={123} />)
     cy.matchImage()
--- test-utils/cypress-darkmode.ts
+++
@@ -1,9 +0,0 @@
-export function stubDarkMode(cy: Cypress.cy) {
-  cy.on('window:before:load', (win) => {
-    cy.stub(win, 'matchMedia')
-      .withArgs('(prefers-color-scheme: dark)')
-      .returns({
-        matches: true,
-      })
-  })
-}
