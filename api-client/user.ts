b809caed8cfd3e33c409322f0aa59fa369f9a3fc
Kazuki Oishi <oishikazuki@gmail.com>
2023-07-17 05:00:58 +0900

    Removed store

--- api-client/user.ts
+++ api-client/user.ts
@@ -1,11 +1,9 @@
-import { AppDispatch } from 'store'
 import { createGraphQLSDK } from 'graphql/client'
 import { Headers } from 'api-client/types'
 
 const invalidateTag = ['user']
 
 export async function getCurrentUserState(
-  dispatch: AppDispatch,
   headers?: Headers,
 ) {
   const sdk = createGraphQLSDK({
