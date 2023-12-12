48a885baaa56c7d1ef66799980a61a9cf17dd900
Kazuki Oishi <oishikazuki@gmail.com>
2023-12-12 09:10:33 +0900

    ダークテーマ設定時の読み込み中のちらつきを修正する

--- components/common/GlobalThemeHandler.tsx
+++ components/common/GlobalThemeHandler.tsx
@@ -1,22 +1,38 @@
 'use client'
 
 import { useDarkTheme } from 'libs/dom/useDarkTheme'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { applyMode, Mode } from '@cloudscape-design/global-styles'
 import { createGlobalStyle } from 'styled-components'
 
-// FIXME: Workaround for cloudscape-design/components bugs for scrolling in small screen
-const GlobalStyle = createGlobalStyle`
+const styled = { createGlobalStyle }
+
+const GlobalStyle = styled.createGlobalStyle<{ appHasHydrated: boolean }>`
   body {
+    // FIXME: Workaround for cloudscape-design/components bugs for scrolling in small screen
     overflow: unset !important;
+
+    @media (prefers-color-scheme: dark) {
+      background-color: #0f1b2a !important;
+    }
+  }
+
+  main {
+    @media (prefers-color-scheme: dark) {
+      // FIXME: UIがちらつくので、読み込み中には非表示にして見えないようにする
+      ${(v) => !v.appHasHydrated && 'opacity: 0 !important;'}
+    }
   }
 `
 
 export function GlobalThemeHandler() {
   const isDarkTheme = useDarkTheme()
+  const [appHasHydrated, setAppHasHydrated] = useState(false)
+
   useEffect(() => {
+    setAppHasHydrated(true)
     applyMode(isDarkTheme ? Mode.Dark : Mode.Light)
   }, [isDarkTheme])
 
-  return <GlobalStyle />
+  return <GlobalStyle appHasHydrated={appHasHydrated} />
 }
