df9f3ab33607b436a99405218d281a193f5d94e8
Kazuki Oishi <oishikazuki@gmail.com>
2023-12-14 03:20:36 +0900

    fix

--- components/common/GlobalThemeHandler.tsx
+++ components/common/GlobalThemeHandler.tsx
@@ -20,7 +20,7 @@ const GlobalStyle = styled.createGlobalStyle<{ appHasHydrated: boolean }>`
   main {
     @media (prefers-color-scheme: dark) {
       // FIXME: UIがちらつくので、読み込み中には非表示にして見えないようにする
-      opacity: ${(v) => (v.appHasHydrated ? 'unset' : '0 !important')};
+      opacity: ${(v) => (v.appHasHydrated ? 'unset !important' : '0')};
     }
   }
 `
